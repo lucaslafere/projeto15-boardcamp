9a7c9e2f9147465a94eac98b952e9b379a0504e5
Lucas Lafèré <laferelucas@gmail.com>
2022-07-22 17:22:53 -0300

    feat: added post/categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,4 +1,5 @@
 import connection from '../db/database.js';
+import { categoryPostSchema } from '../schemas/categoriesSchema.js';
 
 export async function getCategories (req, res) {
     try {
@@ -11,8 +12,17 @@ export async function getCategories (req, res) {
 
 export async function postCategories (req, res) {
     const body = req.body;
+    const { error } = categoryPostSchema.validate(body);
+    if (error) return res.sendStatus(400);
+
     try {
-        
+        const checkExistingCategory = await connection.query(`SELECT * FROM categories WHERE name='${body.name}'`);
+        if (checkExistingCategory.rowCount > 0)  {
+            return res.status(409).send("Essa categoria já existe");
+        }
+        const createCategory = await connection.query(`INSERT INTO categories (name) VALUES ('${body.name}')`);
+        return res.sendStatus(201);      
+
     } catch (error) {
         return res.status(500).send("Server failed");
     }
--- src/routes/categoriesRouter.js
+++ src/routes/categoriesRouter.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { getCategories } from '../controllers/categoriesController.js';
+import { getCategories, postCategories } from '../controllers/categoriesController.js';
 
 const router = Router();
 router.get('/categories', getCategories);
-router.post('/categories');
+router.post('/categories', postCategories);
 
 export default router;
