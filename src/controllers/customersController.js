b67c6881b0c80ad0eaf9169f73d5e13172c870e0
Lucas Lafèré <laferelucas@gmail.com>
2022-07-24 12:22:35 -0300

    fix: fixed bug in get/customers/:id

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -26,12 +26,19 @@ export async function getCustomers (req, res) {
 export async function getCustomersById (req, res) {
     const { id } = req.params;
     try {
-        const query = `
-        SELECT * FROM customers WHERE id=$1`
-        const { rows: customers } = await connection.query(query, [id]);
-        return res.send(customers[0]);
+        const checkId = await connection.query(`
+        SELECT * FROM customers WHERE id=$1`, [id])
+        if (checkId.rowCount === 0) return res.sendStatus(404);
+        else {
+            const { rows: customers } = await connection.query(`
+            SELECT * FROM customers WHERE id=$1`, [id]);
+            return res.send(customers[0]);
+        }
     } catch (error) {
         return res.status(500).send(error);
     }
-    
+}
+
+export async function postCustomers (req, res) {
+
 }
